02c32aed3ac7d2811f286336b574e38cc9b4bcd0
Raj-0908 <104252591+Raj-0908@users.noreply.github.com>
2025-05-01 14:44:58 +0530

    Update authController.js

---
+++ blog-rbac-backend/controllers/authController.js
@@ -0,0 +1,29 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+const generateToken = (user) => {
+  return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
+    expiresIn: "7d"
+  });
+};
+
+exports.signup = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const user = await User.create({ name, email, password });
+    res.status(201).json({ message: "User registered" });
+  } catch {
+    res.status(400).json({ message: "User already exists" });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user || !(await user.matchPassword(password))) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  const token = generateToken(user);
+  res.json({ token });
+};
