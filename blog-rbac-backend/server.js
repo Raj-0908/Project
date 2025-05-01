3c21cd46f494ca10bf45dc760587378261b68332
Raj-0908 <104252591+Raj-0908@users.noreply.github.com>
2025-05-01 14:48:19 +0530

    Update server.js

---
+++ blog-rbac-backend/server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
+const postRoutes = require("./routes/postRoutes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", authRoutes);
+app.use("/api", postRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running on port ${process.env.PORT}`)
+    );
+  })
+  .catch(err => console.error("MongoDB error:", err));
